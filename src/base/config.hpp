eca7f8c9bd2cc363751c4a27cc70c0dbf1172e4e
Daniel <daniel@heshu.by>
2019-10-30 10:54:12 +0300

    config.hpp changed

--- src/base/config.hpp
+++ src/base/config.hpp
@@ -3,8 +3,17 @@
 namespace base::config
 {
 
+//common configuration values
 constexpr bool IS_DEBUG = true;
 
-constexpr const char * const OS_NAME = "Linux";
+constexpr const char* const OS_NAME = "Linux";
+
+// logging configuration
+
+static constexpr const char* LOG_FILE_FORMAT = "app_%m-%d-%Y_%H:%M.log";
+static constexpr const char* LOG_FOLDER = "logs";
+static constexpr const int LOG_FILE_MAX_SIZE = 6 * 1024 * 1024;
+static constexpr const int LOG_FILE_MIN_SPACE = 100 * 1024 * 1024;
+static constexpr const int MAX_LOG_FILE_COUNT = 512;
 
 }
