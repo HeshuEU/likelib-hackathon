b046034d89ce35a938d424ef8aeb1f7dd5f1373a
Daniel <daniel@heshu.by>
2019-10-31 16:40:32 +0300

    more serious node main

--- src/node/main.cpp
+++ src/node/main.cpp
@@ -5,9 +5,9 @@
 int main()
 {
     try {
-        base::initLog(base::LogLevel::ALL, base::Sink::STDOUT);
-        LOG_INFO << "abcdef";
-        throw std::runtime_error{"42"};
+        base::initLog(base::LogLevel::ALL, base::Sink::STDOUT | base::Sink::FILE);
+
+        LOG_INFO << "Application startup";
     }
     catch(const std::exception& error) {
         LOG_ERROR << "exception caught in main: " << error.what();
