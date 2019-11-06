c98c2f79ee021b2471123be2d27f8bb3fc6ba239
Daniel <daniel@heshu.by>
2019-11-06 19:45:15 +0300

    signal handling added

--- src/node/main.cpp
+++ src/node/main.cpp
@@ -1,21 +1,60 @@
+#include "base/config.hpp"
 #include "base/log.hpp"
 #include "base/assert.hpp"
 
+#ifdef CONFIG_OS_FAMILY_UNIX
+#include <cstring>
+#endif
+
+#include <csignal>
+#include <cstdlib>
 #include <exception>
+
+
+namespace {
+
+
+extern "C" void signalHandler(int signal) {
+    LOG_INFO << "Signal caught: " << signal
+                                 #ifdef CONFIG_OS_FAMILY_UNIX
+                                     << " (" << strsignal(signal) << ")"
+                                 #endif
+                                 #ifdef CONFIG_IS_DEBUG
+                                     << '\n' << boost::stacktrace::stacktrace()
+                                 #endif
+                                 ;
+}
+
+void atExitHandler()
+{
+    LOG_INFO << "atExitHandler called";
+}
+
+}
 
 int main(int argc, char** argv)
 {
     try {
         base::initLog(base::LogLevel::ALL, base::Sink::STDOUT | base::Sink::FILE);
-
-        CHECK(argc > 1);
-
         LOG_INFO << "Application startup";
+
+        std::signal(SIGTERM, signalHandler);
+        std::signal(SIGSEGV, signalHandler);
+        std::signal(SIGINT, signalHandler);
+        std::signal(SIGILL, signalHandler);
+        std::signal(SIGABRT, signalHandler);
+        std::signal(SIGFPE, signalHandler);
+
+        CHECK_SOFT(std::atexit(atExitHandler) == 0);
+
+        while(true) {
+
+        }
     }
     catch(const std::exception& error) {
-        LOG_ERROR << "exception caught in main: " << error.what();
+        LOG_ERROR << "Exception caught in main: " << error.what();
     }
     catch(...) {
-        LOG_ERROR << "unknown exception caught";
+        LOG_ERROR << "Unknown exception caught";
     }
 }
