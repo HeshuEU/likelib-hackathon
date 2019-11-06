84c2b8d0050c9a814be58bc7c80a9bd36bed97fd
Daniel <daniel@heshu.by>
2019-11-06 17:59:10 +0300

    assert now exits with special exit code on failure

--- src/base/assert.hpp
+++ src/base/assert.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
 #include "base/config.hpp"
+#include "base/log.hpp"
+
+#include <exception>
 
 #ifdef CONFIG_IS_DEBUG
-    #define CHECK(condition, message) \
-        throw base::Error{message}
+    #define CHECK(condition) \
+        if(!(condition)) { \
+            LOG_WARNING << "Assertion failed: " << #condition; \
+            std::exit(base::config::EXIT_ASSERT_FAILED); \
+        }
 #else
     #define CHECK(condition, message) do {} while(0)
 #endif
