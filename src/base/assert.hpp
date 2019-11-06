09066ca0ccdc5697d4434376c4c1c9b08fbe5f49
Daniel <daniel@heshu.by>
2019-11-06 19:44:39 +0300

    stacktrace added

--- src/base/assert.hpp
+++ src/base/assert.hpp
@@ -1,18 +1,32 @@
 #pragma once
 
 #include "base/config.hpp"
+
+#ifdef CONFIG_IS_DEBUG
+
 #include "base/log.hpp"
+
+#include <boost/stacktrace.hpp>
 
 #include <exception>
 
-#ifdef CONFIG_IS_DEBUG
 #define CHECK(condition) \
     if(!(condition)) { \
-        LOG_WARNING << "Assertion failed: " << #condition; \
+        LOG_WARNING << "Assertion failed: " << #condition << '\n' << boost::stacktrace::stacktrace(); \
         std::exit(base::config::EXIT_ASSERT_FAILED); \
     }
+
+#define CHECK_SOFT(condition) \
+    if(!(condition)) { \
+        LOG_WARNING << "Soft check failed: " << #condition << '\n' << boost::stacktrace::stacktrace(); \
+    }
+
 #else
 #define CHECK(condition, message) \
+    do { \
+    } while(0)
+
+#define CHECK_SOFT(condition, message) \
     do { \
     } while(0)
 #endif
