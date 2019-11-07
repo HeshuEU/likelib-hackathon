4bc444a80bfca66b923fd4291e878ea4603e64ee
Daniel <daniel@heshu.by>
2019-11-07 14:06:57 +0300

    renamed macro CHECK to ASSERT

--- src/base/assert.hpp
+++ src/base/assert.hpp
@@ -10,23 +10,23 @@
 
 #include <exception>
 
-#define CHECK(condition) \
+#define ASSERT(condition) \
     if(!(condition)) { \
         LOG_WARNING << "Assertion failed: " << #condition << '\n' << boost::stacktrace::stacktrace(); \
         std::exit(base::config::EXIT_ASSERT_FAILED); \
     }
 
-#define CHECK_SOFT(condition) \
+#define ASSERT_SOFT(condition) \
     if(!(condition)) { \
         LOG_WARNING << "Soft check failed: " << #condition << '\n' << boost::stacktrace::stacktrace(); \
     }
 
 #else
-#define CHECK(condition, message) \
+#define ASSERT(condition, message) \
     do { \
     } while(0)
 
-#define CHECK_SOFT(condition, message) \
+#define ASSERT_SOFT(condition, message) \
     do { \
     } while(0)
 #endif
