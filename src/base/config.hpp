7bcfe497585043727c79e7ad54b5d4d9b7fd309a
Daniel <daniel@heshu.by>
2019-11-09 23:14:10 +0300

    changed log files mask

--- src/base/config.hpp
+++ src/base/config.hpp
@@ -57,7 +57,7 @@ constexpr const char* const OS_NAME =
 
 
 // logging configuration
-static constexpr const char* LOG_FILE_FORMAT = "app_%m-%d-%Y_%H:%M.log";
+static constexpr const char* LOG_FILE_FORMAT = "%m-%d-%Y_%H:%M.log";
 static constexpr const char* LOG_FOLDER = "logs";
 static constexpr const int LOG_FILE_MAX_SIZE = 6 * 1024 * 1024;
 static constexpr const int LOG_FILE_MIN_SPACE = 100 * 1024 * 1024;
