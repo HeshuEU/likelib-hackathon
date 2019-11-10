c52fd0899a7b21bd02de2bdf5111a35ea8971c29
Daniel <daniel@heshu.by>
2019-11-10 16:03:49 +0300

    added hashing to base. functions are non-implemented yet

---
+++ src/base/hash.cpp
@@ -0,0 +1,25 @@
+#include "hash.hpp"
+
+#include "base/assert.hpp"
+
+namespace base
+{
+
+base::Bytes sha256(const base::Bytes& data)
+{
+    base::Bytes ret{256 / 8};
+
+    ASSERT(ret.size() == 32);
+    return ret;
+}
+
+
+base::Bytes sha1(const base::Bytes& data);
+{
+    base::Bytes ret(160 / 8);
+
+    ASSERT(ret.size() == 20);
+    return ret;
+}
+
+}
---
+++ src/base/hash.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "base/bytes.hpp"
+
+namespace base
+{
+
+base::Bytes sha256(const base::Bytes& data);
+base::Bytes sha1(const base::Bytes& data);
+
+}
