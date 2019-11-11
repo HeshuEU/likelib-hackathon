b590a6eb357e534fe94e9b657cd3a41f81a198ac
Daniel <daniel@heshu.by>
2019-11-11 17:28:04 +0300

    network::Package serialization test

---
+++ test/network/packet.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/unit_test.hpp>
+
+#include "network/packet.hpp"
+
+BOOST_AUTO_TEST_CASE(packet_serialization)
+{
+    network::Packet packet1(0xFF);
+    base::Bytes serialized = packet1.serialize();
+    network::Packet packet2 = network::Packet::deserialize(serialized);
+    BOOST_CHECK(packet1 == packet2);
+}
