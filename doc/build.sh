a77ce89aa281c96179dd96d23835d94aa54f09f7
Daniel <daniel@heshu.by>
2019-11-15 21:17:45 +0300

    patched vcpkg install script and build script to install to /opt directory and work with it

--- doc/build.sh
+++ doc/build.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ../build
 cd ../build
-cmake .. "-DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake .. "-DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake"
--- doc/prepare_build.sh
+++ doc/prepare_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd ~
+FOLDER=${PWD}
+cd /opt || exit 1
 git clone --recurse-submodules https://github.com/Microsoft/vcpkg.git
-cd ./vcpkg/
-./bootstrap-vcpkg.sh
-sudo apt-get install curl unzip tar
+cd ./vcpkg || exit 1
+apt-get install curl unzip tar g++
 ./bootstrap-vcpkg.sh
 ./vcpkg install boost
 ./vcpkg install grpc
 ./vcpkg install openssl
-
+cd "${FOLDER}" || exit 1
