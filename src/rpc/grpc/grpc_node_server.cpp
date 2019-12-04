5126398a32f988c9060a11c36fb1d003b3767db8
Siarhei Sadouski <sadovski@heshu.by>
2019-12-04 13:49:29 +0300

    Fix compilation warning

--- src/rpc/grpc/grpc_node_server.cpp
+++ src/rpc/grpc/grpc_node_server.cpp
@@ -4,7 +4,7 @@ namespace rpc
 {
 
 GrpcNodeServer::GrpcNodeServer(const std::string& server_address, std::shared_ptr<BaseRpc> service)
-    :  _server_address(server_address), _service()
+    :  _service(),  _server_address(server_address)
 {
     _service.init(service);
 }
