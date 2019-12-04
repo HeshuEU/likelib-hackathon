ccfd76ec2b6ef61d74f7a3ce68aa1ec2ed0fe4f3
Siarhei Sadouski <sadovski@heshu.by>
2019-12-04 12:43:33 +0300

    Added constructors, compare operators and serialization

--- src/base/time.cpp
+++ src/base/time.cpp
@@ -3,26 +3,38 @@
 namespace base
 {
 
-Time::Time()
-{}
-
 Time::Time(std::chrono::time_point<std::chrono::system_clock> time_point) : _time(time_point)
 {}
+
 
 std::uint_least32_t Time::millisecondsInEpoch() const
 {
     return std::chrono::duration_cast<std::chrono::milliseconds>(_time.time_since_epoch()).count();
 }
 
+
 std::uint_least32_t Time::secondsInEpoch() const
 {
     return std::chrono::duration_cast<std::chrono::seconds>(_time.time_since_epoch()).count();
 }
 
+bool Time::operator==(const Time& other) const
+{
+    return _time == other._time;
+}
+
+
+bool Time::operator!=(const Time& other) const
+{
+    return !(*this == other);
+}
+
+
 Time Time::now()
 {
     return Time(std::chrono::system_clock::now());
 }
+
 
 Time Time::fromMilliseconds(std::uint_least32_t milliseconds_from_epoch)
 {
@@ -30,10 +42,27 @@ Time Time::fromMilliseconds(std::uint_least32_t milliseconds_from_epoch)
     return Time(std::chrono::time_point<std::chrono::system_clock>(duration_from_epoch));
 }
 
+
 Time Time::fromSeconds(std::uint_least32_t seconds_from_epoch)
 {
     std::chrono::duration<std::uint_least32_t, std::ratio<1, 1>> duration_from_epoch(seconds_from_epoch);
     return Time(std::chrono::time_point<std::chrono::system_clock>(duration_from_epoch));
 }
+
+
+::base::SerializationIArchive& operator>>(::base::SerializationIArchive& ia, Time& time)
+{
+    std::uint_least32_t timestamp;
+    ia >> timestamp;
+    time = base::Time::fromMilliseconds(timestamp);
+    return ia;
+}
+
+
+::base::SerializationOArchive& operator<<(::base::SerializationOArchive& oa, const Time& time)
+{
+    return oa << time.millisecondsInEpoch();
+}
+
 
 } // namespace base
--- src/base/time.hpp
+++ src/base/time.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "base/serialization.hpp"
+
 #include <chrono>
 
 namespace base
@@ -8,22 +10,32 @@ namespace base
 class Time
 {
   public:
-    Time();
-
+    Time() = default;
     Time(std::chrono::time_point<std::chrono::system_clock> time_point);
-
+    Time(const Time&) = default;
+    Time(Time&&) = default;
+    //====================
+    Time& operator=(const Time&) = default;
+    Time& operator=(Time&&) = default;
+    //====================
+    ~Time() = default;
+    //====================
     std::uint_least32_t millisecondsInEpoch() const;
-
     std::uint_least32_t secondsInEpoch() const;
-
+    //====================
+    bool operator==(const Time& other) const;
+    bool operator!=(const Time& other) const;
+    //====================
     static Time now();
-
     static Time fromMilliseconds(std::uint_least32_t milliseconds_from_epoch);
-
     static Time fromSeconds(std::uint_least32_t seconds_from_epoch);
-
+    //====================
   private:
     std::chrono::time_point<std::chrono::system_clock> _time;
+    //=====================
 };
+
+::base::SerializationIArchive& operator>>(::base::SerializationIArchive& ia, Time& tx);
+::base::SerializationOArchive& operator<<(::base::SerializationOArchive& oa, const Time& tx);
 
 } // namespace base
