5fee65b8e62c70cc4126b8af347563e36f0b70ca
Oleg Malyshev <malyshev@heshu.by>
2019-12-06 13:28:37 +0300

    added test files to cmake and fixed the tests for the timer

--- test/base/timer.cpp
+++ test/base/timer.cpp
@@ -3,16 +3,17 @@
 #include "base/timer.hpp"
 
 #include <thread>
+#include <iostream>
 
-constexpr std::int64_t milli_fault = 10;
+constexpr std::int64_t milli_fault = 3;
 
 BOOST_AUTO_TEST_CASE(time_usage1)
 {
     base::Timer timer;
     timer.start();
-    std::this_thread::sleep_for(std::chrono::microseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
     auto milli = timer.elapsedMillis();
-    BOOST_CHECK(milli >= 100 && milli < 100 + milli_fault);
+    BOOST_CHECK(milli >= 100 && milli < (100 + milli_fault));
 }
 
 
@@ -21,7 +22,7 @@ BOOST_AUTO_TEST_CASE(time_usage2)
     base::Timer timer1;
     base::Timer timer2;
     timer1.start(); timer2.start();
-    std::this_thread::sleep_for(std::chrono::microseconds(213));
+    std::this_thread::sleep_for(std::chrono::milliseconds(213));
     auto milli1 = timer1.elapsedMillis();
     auto milli2 = timer2.elapsedMillis();
     BOOST_CHECK(abs(milli1 - milli2) < milli_fault);
@@ -32,7 +33,7 @@ BOOST_AUTO_TEST_CASE(time_constructor_copy)
 {
     base::Timer timer1;
     timer1.start();
-    std::this_thread::sleep_for(std::chrono::microseconds(149));
+    std::this_thread::sleep_for(std::chrono::milliseconds(149));
     base::Timer timer2 = timer1;
     auto milli1 = timer1.elapsedMillis();
     auto milli2 = timer2.elapsedMillis();
@@ -44,6 +45,6 @@ BOOST_AUTO_TEST_CASE(time_elapsed_seconds)
 {
     base::Timer timer;
     timer.start();
-    std::this_thread::sleep_for(std::chrono::microseconds(1149));
-    BOOST_CHECK(abs(timer.elapsedSeconds() - 1.15) < 0.01);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1149));
+    BOOST_CHECK(abs(timer.elapsedSeconds() - 1.15) < 0.03);
 }
