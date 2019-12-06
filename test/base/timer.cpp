aef4a003d5695ed2b0d09b9de191ce71de41be7c
Oleg Malyshev <malyshev@heshu.by>
2019-12-06 13:40:23 +0300

    reduced the time in the tests for the timer

--- test/base/timer.cpp
+++ test/base/timer.cpp
@@ -11,9 +11,9 @@ BOOST_AUTO_TEST_CASE(time_usage1)
 {
     base::Timer timer;
     timer.start();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(30));
     auto milli = timer.elapsedMillis();
-    BOOST_CHECK(milli >= 100 && milli < (100 + milli_fault));
+    BOOST_CHECK(milli >= 30 && milli < (30 + milli_fault));
 }
 
 
@@ -22,7 +22,7 @@ BOOST_AUTO_TEST_CASE(time_usage2)
     base::Timer timer1;
     base::Timer timer2;
     timer1.start(); timer2.start();
-    std::this_thread::sleep_for(std::chrono::milliseconds(213));
+    std::this_thread::sleep_for(std::chrono::milliseconds(56));
     auto milli1 = timer1.elapsedMillis();
     auto milli2 = timer2.elapsedMillis();
     BOOST_CHECK(abs(milli1 - milli2) < milli_fault);
@@ -33,8 +33,20 @@ BOOST_AUTO_TEST_CASE(time_constructor_copy)
 {
     base::Timer timer1;
     timer1.start();
-    std::this_thread::sleep_for(std::chrono::milliseconds(149));
+    std::this_thread::sleep_for(std::chrono::milliseconds(49));
     base::Timer timer2 = timer1;
+    auto milli1 = timer1.elapsedMillis();
+    auto milli2 = timer2.elapsedMillis();
+    BOOST_CHECK(abs(milli1 - milli2) < milli_fault);
+}
+
+
+BOOST_AUTO_TEST_CASE(time_constructor_move)
+{
+    base::Timer timer1;
+    timer1.start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(49));
+    base::Timer timer2 = std::move(timer1);
     auto milli1 = timer1.elapsedMillis();
     auto milli2 = timer2.elapsedMillis();
     BOOST_CHECK(abs(milli1 - milli2) < milli_fault);
@@ -45,6 +57,6 @@ BOOST_AUTO_TEST_CASE(time_elapsed_seconds)
 {
     base::Timer timer;
     timer.start();
-    std::this_thread::sleep_for(std::chrono::milliseconds(1149));
-    BOOST_CHECK(abs(timer.elapsedSeconds() - 1.15) < 0.03);
+    std::this_thread::sleep_for(std::chrono::milliseconds(149));
+    BOOST_CHECK(abs(timer.elapsedSeconds() - 0.15) < 0.01);
 }
