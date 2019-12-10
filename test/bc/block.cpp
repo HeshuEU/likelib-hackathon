ee60189b3454fb33dbc4fc7c8fa76274e07e77b8
Oleg Malyshev <malyshev@heshu.by>
2019-12-10 16:52:06 +0300

    changed the tests for Block

--- test/bc/block.cpp
+++ test/bc/block.cpp
@@ -8,11 +8,15 @@ namespace
 bc::TransactionsSet getTestSet()
 {
     bc::TransactionsSet set;
-    set.add(bc::Transaction(bc::Address("vjS247DGFSv\n "), bc::Address("()#%DSOJ\n"), 12398, base::Time()));
-    set.add(bc::Transaction(bc::Address("vj^Hs47DGFSv\n "), bc::Address("()#%Dsdg\n"), 5825285, base::Time()));
-    set.add(bc::Transaction(bc::Address("vjS2%#&DGF\n "), bc::Address("()#%DdfOJ\n"), 12245398, base::Time()));
-    set.add(bc::Transaction(bc::Address("vjS247sdgFSv\n "), bc::Address("{#%DSOJ "), 168524347, base::Time()));
-    set.add(bc::Transaction(bc::Address("vjS2  DGFSv\n "), bc::Address("()#%DSdsJ\n"), 1434457, base::Time()));
+    set.add(bc::Transaction(bc::Address("from1 vjS247DGFSv\n "), bc::Address("to1 ()#%DSOJ\n"), 12398, base::Time()));
+    set.add(
+        bc::Transaction(bc::Address("from2 vj^Hs47DGFSv\n "), bc::Address("to2 ()#%Dsdg\n"), 5825285, base::Time()));
+    set.add(
+        bc::Transaction(bc::Address("from3 vjS2%#&DGF\n "), bc::Address("to3 ()#%DdfOJ\n"), 12245398, base::Time()));
+    set.add(
+        bc::Transaction(bc::Address("from4 vjS247sdgFSv\n "), bc::Address("to4 {#%DSOJ "), 168524347, base::Time()));
+    set.add(
+        bc::Transaction(bc::Address("from5 vjS2  DGFSv\n "), bc::Address("to5 ()#%DSdsJ\n"), 1434457, base::Time()));
     return set;
 }
 } // namespace
@@ -38,20 +42,20 @@ BOOST_AUTO_TEST_CASE(block_constructor2)
     auto block_tx_set = block.getTransactions();
 
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247DGFSv\n "), bc::Address("()#%DSOJ\n"), 12398, base::Time())));
+        bc::Transaction(bc::Address("from1 vjS247DGFSv\n "), bc::Address("to1 ()#%DSOJ\n"), 12398, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vj^Hs47DGFSv\n "), bc::Address("()#%Dsdg\n"), 5825285, base::Time())));
+        bc::Transaction(bc::Address("from2 vj^Hs47DGFSv\n "), bc::Address("to2 ()#%Dsdg\n"), 5825285, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS2%#&DGF\n "), bc::Address("()#%DdfOJ\n"), 12245398, base::Time())));
+        bc::Transaction(bc::Address("from3 vjS2%#&DGF\n "), bc::Address("to3 ()#%DdfOJ\n"), 12245398, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247sdgFSv\n "), bc::Address("{#%DSOJ "), 168524347, base::Time())));
+        bc::Transaction(bc::Address("from4 vjS247sdgFSv\n "), bc::Address("to4 {#%DSOJ "), 168524347, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS2  DGFSv\n "), bc::Address("()#%DSdsJ\n"), 1434457, base::Time())));
+        bc::Transaction(bc::Address("from5 vjS2  DGFSv\n "), bc::Address("to5 ()#%DSdsJ\n"), 1434457, base::Time())));
 
-    BOOST_CHECK(!block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247DGFSv\n "), bc::Address("()#%DSOJ\n"), 146987, base::Time())));
-    BOOST_CHECK(!block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247DG\n "), bc::Address("()#%DOJ\n"), 12398, base::Time::now())));
+    BOOST_CHECK(
+        !block_tx_set.find(bc::Transaction(bc::Address("vjS247DGFSv\n "), bc::Address("()#%DSOJ\n"), 146987, base::Time())));
+    BOOST_CHECK(
+        !block_tx_set.find(bc::Transaction(bc::Address("vjS247DG\n "), bc::Address("()#%DOJ\n"), 12398, base::Time::now())));
 }
 
 
@@ -64,20 +68,20 @@ BOOST_AUTO_TEST_CASE(block_constructor3)
     auto block_tx_set = block.getTransactions();
 
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247DGFSv\n "), bc::Address("()#%DSOJ\n"), 12398, base::Time())));
+        bc::Transaction(bc::Address("from1 vjS247DGFSv\n "), bc::Address("to1 ()#%DSOJ\n"), 12398, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vj^Hs47DGFSv\n "), bc::Address("()#%Dsdg\n"), 5825285, base::Time())));
+        bc::Transaction(bc::Address("from2 vj^Hs47DGFSv\n "), bc::Address("to2 ()#%Dsdg\n"), 5825285, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS2%#&DGF\n "), bc::Address("()#%DdfOJ\n"), 12245398, base::Time())));
+        bc::Transaction(bc::Address("from3 vjS2%#&DGF\n "), bc::Address("to3 ()#%DdfOJ\n"), 12245398, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247sdgFSv\n "), bc::Address("{#%DSOJ "), 168524347, base::Time())));
+        bc::Transaction(bc::Address("from4 vjS247sdgFSv\n "), bc::Address("to4 {#%DSOJ "), 168524347, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS2  DGFSv\n "), bc::Address("()#%DSdsJ\n"), 1434457, base::Time())));
+        bc::Transaction(bc::Address("from5 vjS2  DGFSv\n "), bc::Address("to5 ()#%DSdsJ\n"), 1434457, base::Time())));
 
-    BOOST_CHECK(!block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247DGFSv\n "), bc::Address("()#%DSOJ\n"), 146987, base::Time())));
-    BOOST_CHECK(!block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247DG\n "), bc::Address("()#%DOJ\n"), 12398, base::Time::now())));
+    BOOST_CHECK(
+        !block_tx_set.find(bc::Transaction(bc::Address("vjS247DGFSv\n "), bc::Address("()#%DSOJ\n"), 146987, base::Time())));
+    BOOST_CHECK(
+        !block_tx_set.find(bc::Transaction(bc::Address("vjS247DG\n "), bc::Address("()#%DOJ\n"), 12398, base::Time::now())));
 }
 
 
@@ -85,8 +89,8 @@ BOOST_AUTO_TEST_CASE(block_sets_all)
 {
     bc::Block block(base::Bytes("#%*(D VASGL/n\n\f asdeGDH#%"), getTestSet());
     auto tx_set = getTestSet();
-    tx_set.remove(bc::Transaction(bc::Address("vjS2%#&DGF\n "), bc::Address("()#%DdfOJ\n"), 12245398, base::Time()));
-    tx_set.remove(bc::Transaction(bc::Address("vjS2  DGFSv\n "), bc::Address("()#%DSdsJ\n"), 1434457, base::Time()));
+    tx_set.remove(bc::Transaction(bc::Address("from3 vjS2%#&DGF\n "), bc::Address("to3 ()#%DdfOJ\n"), 12245398, base::Time()));
+    tx_set.remove(bc::Transaction(bc::Address("from5 vjS2  DGFSv\n "), bc::Address("to5 ()#%DSdsJ\n"), 1434457, base::Time()));
     tx_set.add(bc::Transaction(bc::Address("SD#%),/n\' \n"), bc::Address("#(vm496LDF "), 67805678, base::Time()));
     block.setNonce(bc::NonceInt(5678969));
     block.setPrevBlockHash(base::Bytes("SDGK\nsfj$^DG Ldfj34/GHOJ "));
@@ -97,16 +101,16 @@ BOOST_AUTO_TEST_CASE(block_sets_all)
     auto block_tx_set = block.getTransactions();
 
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247DGFSv\n "), bc::Address("()#%DSOJ\n"), 12398, base::Time())));
+        bc::Transaction(bc::Address("from1 vjS247DGFSv\n "), bc::Address("to1 ()#%DSOJ\n"), 12398, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vj^Hs47DGFSv\n "), bc::Address("()#%Dsdg\n"), 5825285, base::Time())));
+        bc::Transaction(bc::Address("from2 vj^Hs47DGFSv\n "), bc::Address("to2 ()#%Dsdg\n"), 5825285, base::Time())));
     BOOST_CHECK(block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247sdgFSv\n "), bc::Address("{#%DSOJ "), 168524347, base::Time())));
+        bc::Transaction(bc::Address("from4 vjS247sdgFSv\n "), bc::Address("to4 {#%DSOJ "), 168524347, base::Time())));
     BOOST_CHECK(block_tx_set.find(
         bc::Transaction(bc::Address("SD#%),/n\' \n"), bc::Address("#(vm496LDF "), 67805678, base::Time())));
 
     BOOST_CHECK(!block_tx_set.find(
-        bc::Transaction(bc::Address("vjS2%#&DGF\n "), bc::Address("()#%DdfOJ\n"), 12245398, base::Time())));
+        bc::Transaction(bc::Address("from3 vjS2%#&DGF\n "), bc::Address("to3 ()#%DdfOJ\n"), 12245398, base::Time())));
     BOOST_CHECK(!block_tx_set.find(
-        bc::Transaction(bc::Address("vjS247DG\n "), bc::Address("()#%DOJ\n"), 12398, base::Time::now())));
+        bc::Transaction(bc::Address("from5 vjS2  DGFSv\n "), bc::Address("to5 ()#%DSdsJ\n"), 1434457, base::Time())));
 }
