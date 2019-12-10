66cf41321316192ab45f83b8c05aaf362cdb6371
Oleg Malyshev <malyshev@heshu.by>
2019-12-10 12:16:02 +0300

    the tests for Transaction

--- test/bc/transaction.cpp
+++ test/bc/transaction.cpp
@@ -27,3 +27,55 @@ BOOST_AUTO_TEST_CASE(transaction_constructor2)
     BOOST_CHECK(tx.getTimestamp() == base::Time::now());
     BOOST_CHECK(tx.getAmount() == 1239823409);
 }
+
+
+BOOST_AUTO_TEST_CASE(transaction_constructor_copy)
+{
+    bc::Transaction tx1(bc::Address("vjS#%(247DGFSMKv\n sdf?//"), bc::Address("()#%9vdmLDSOJ\n\n\\/Skg/dfe"), 1239823409, base::Time::now());
+    bc::Transaction tx2(tx1);
+
+    BOOST_CHECK(tx2.getFrom() == bc::Address("vjS#%(247DGFSMKv\n sdf?//"));
+    BOOST_CHECK(tx2.getTo() == bc::Address("()#%9vdmLDSOJ\n\n\\/Skg/dfe"));
+    BOOST_CHECK(tx2.getTimestamp() == base::Time::now());
+    BOOST_CHECK(tx2.getAmount() == 1239823409);
+}
+
+
+BOOST_AUTO_TEST_CASE(transaction_constructor_move)
+{
+    bc::Transaction tx1(bc::Address("vjS#%(247DGFSMKv\n sdf?//"), bc::Address("()#%9vdmLDSOJ\n\n\\/Skg/dfe"), 1239823409, base::Time::now());
+    bc::Transaction tx2(std::move(tx1));
+
+    BOOST_CHECK(tx2.getFrom() == bc::Address("vjS#%(247DGFSMKv\n sdf?//"));
+    BOOST_CHECK(tx2.getTo() == bc::Address("()#%9vdmLDSOJ\n\n\\/Skg/dfe"));
+    BOOST_CHECK(tx2.getTimestamp() == base::Time::now());
+    BOOST_CHECK(tx2.getAmount() == 1239823409);
+}
+
+
+BOOST_AUTO_TEST_CASE(transaction_operator_equal_copy)
+{
+    bc::Transaction tx1(bc::Address("vjS#%(247DGFSMKv\n sdf?//"), bc::Address("()#%9vdmLDSOJ\n\n\\/Skg/dfe"), 1239823409, base::Time::now());
+    bc::Transaction tx2(bc::Address("vjSSDGHS*#%/fg\f"), bc::Address("()#%sdo#%KGD\n/Skg/dfe"), 821481368, base::Time::now());
+
+    BOOST_CHECK(tx1 != tx2);
+    tx2 = tx1;
+    BOOST_CHECK(tx2.getFrom() == bc::Address("vjS#%(247DGFSMKv\n sdf?//"));
+    BOOST_CHECK(tx2.getTo() == bc::Address("()#%9vdmLDSOJ\n\n\\/Skg/dfe"));
+    BOOST_CHECK(tx2.getTimestamp() == base::Time::now());
+    BOOST_CHECK(tx2.getAmount() == 1239823409);
+}
+
+
+BOOST_AUTO_TEST_CASE(transaction_operator_equal_move)
+{
+    bc::Transaction tx1(bc::Address("vjS#%(247DGFSMKv\n sdf?//"), bc::Address("()#%9vdmLDSOJ\n\n\\/Skg/dfe"), 1239823409, base::Time::now());
+    bc::Transaction tx2(bc::Address("vjSSDGHS*#%/fg\f"), bc::Address("()#%sdo#%KGD\n/Skg/dfe"), 821481368, base::Time::now());
+
+    BOOST_CHECK(tx1 != tx2);
+    tx2 = std::move(tx1);
+    BOOST_CHECK(tx2.getFrom() == bc::Address("vjS#%(247DGFSMKv\n sdf?//"));
+    BOOST_CHECK(tx2.getTo() == bc::Address("()#%9vdmLDSOJ\n\n\\/Skg/dfe"));
+    BOOST_CHECK(tx2.getTimestamp() == base::Time::now());
+    BOOST_CHECK(tx2.getAmount() == 1239823409);
+}
