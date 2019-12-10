9bb16ec3c0155d49f230aa381c351da2944359c4
Oleg Malyshev <malyshev@heshu.by>
2019-12-10 17:43:53 +0300

    added a serialization test to TransactionsSet

--- test/bc/transactions_set.cpp
+++ test/bc/transactions_set.cpp
@@ -161,3 +161,26 @@ BOOST_AUTO_TEST_CASE(transactions_set_iterators_usage4)
     }
     BOOST_CHECK(is_ok);
 }
+
+
+BOOST_AUTO_TEST_CASE(transactions_set_serialixation)
+{
+    auto tx_set = getTestSet();
+    base::SerializationOArchive oa;
+    oa << tx_set;
+
+    base::SerializationIArchive ia(oa.getBytes());
+    bc::TransactionsSet tx_set2;
+    ia >> tx_set2;
+
+    BOOST_CHECK(tx_set2.find(
+        bc::Transaction(bc::Address("from1 vjS247DGFSv\n "), bc::Address("to1 ()#%DSOJ\n"), 12398, base::Time())));
+    BOOST_CHECK(tx_set2.find(
+        bc::Transaction(bc::Address("from2 vj^Hs47DGFSv\n "), bc::Address("to2 ()#%Dsdg\n"), 5825285, base::Time())));
+    BOOST_CHECK(tx_set2.find(
+        bc::Transaction(bc::Address("from3 vjS2%#&DGF\n "), bc::Address("to3 ()#%DdfOJ\n"), 12245398, base::Time())));
+    BOOST_CHECK(tx_set2.find(
+        bc::Transaction(bc::Address("from4 vjS247sdgFSv\n "), bc::Address("to4 {#%DSOJ "), 168524347, base::Time())));
+    BOOST_CHECK(tx_set2.find(
+        bc::Transaction(bc::Address("from5 vjS2  DGFSv\n "), bc::Address("to5 ()#%DSdsJ\n"), 1434457, base::Time())));
+}
