a0735c1935b3cc1c101702bc91643de441588af6
Oleg Malyshev <malyshev@heshu.by>
2019-12-16 13:39:25 +0300

    started changing tests, but there's still a bug in the master, so we have to wait for the persistent-storage branch to merge

--- test/integration_test/test_cases/test_transaction.py
+++ test/integration_test/test_cases/test_transaction.py
@@ -8,6 +8,23 @@ import multiprocessing as mp
 _work_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_transaction")
 
 
+def create_and_check_transaction(rpc_client_exec_path, host_address, from_address, to_address, amount, final_amount):
+    pipe = subprocess.run([rpc_client_exec_path, "transfer", "--host", host_address, "--from", from_address, "--to", to_address, "--amount", amount], capture_output=True)
+
+    if pipe.returncode != 0:
+        print(pipe.stderr)
+        exit(1)
+    
+    transfer_time_out = 3
+    time.sleep(transfer_time_out)
+
+    pipe = subprocess.run([rpc_client_exec_path, "get_balance", "--host", host_address, "--address", to_address], capture_output=True)
+    if final_amount.encode() not in pipe.stdout or pipe.returncode != 0:
+        print(pipe.stderr)
+        exit(1)
+
+
+
 def node_run_fun(node_exec_path):   
     node_config_file_content = '''
     {
@@ -36,7 +53,7 @@ def node_run_fun(node_exec_path):
     with open(_node_config_file, 'w') as node_config:
         node_config.write(node_config_file_content)
 
-    test_timeout = 20
+    test_timeout = 15
     try:
         return_code = subprocess.run([node_exec_path, "--config", _node_config_file], capture_output=True, timeout=test_timeout)
     except Exception:
@@ -50,33 +67,14 @@ def client_run_fun(rpc_client_exec_path):
         os.makedirs(work_dir)
     os.chdir(work_dir)
 
-    time_to_node_set_up = 5
+    time_to_node_set_up = 3
     time.sleep(time_to_node_set_up)
 
     host_address = "127.0.0.1:50051"
-    to_address = "11111111111111111111111111111111"
-
-    pipe = subprocess.run([rpc_client_exec_path, "get_balance", "--host", host_address, "--address", to_address], capture_output=True)
-    if b"[0]" not in pipe.stdout or pipe.returncode != 0:
-        print(pipe.stderr)
-        exit(1)
     
-    from_address = "00000000000000000000000000000000"
-    
-    amount = "120"
-    pipe = subprocess.run([rpc_client_exec_path, "transfer", "--host", host_address, "--from", from_address, "--to", to_address, "--amount", amount], capture_output=True)
-
-    if pipe.returncode != 0:
-        print(pipe.stderr)
-        exit(1)
-    
-    transfer_time_out = 10
-    time.sleep(transfer_time_out)
-
-    pipe = subprocess.run([rpc_client_exec_path, "get_balance", "--host", host_address, "--address", to_address], capture_output=True)
-    if amount.encode() not in pipe.stdout or pipe.returncode != 0:
-        print(pipe.stderr)
-        exit(1)
+    create_and_check_transaction(rpc_client_exec_path, host_address, "00000000000000000000000000000000", "Shisha", "1000", "1000")
+    #create_and_check_transaction(rpc_client_exec_path, host_address, "Shisha", "Andre", "670", "670")  #isn't working right now, waiting 
+    #create_and_check_transaction(rpc_client_exec_path, host_address, "Andre", "Shisha", "100", "100")  #for the storage branch to be filled
 
     exit(0)
 
