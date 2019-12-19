e3f09e3b4a9e309224c5d68de5bdd2f47bec2634
Oleg Malyshev <malyshev@heshu.by>
2019-12-19 17:49:43 +0300

    added tests for DataBase

--- test/base/database.cpp
+++ test/base/database.cpp
@@ -30,3 +30,66 @@ BOOST_AUTO_TEST_CASE(data_base_test_1)
 
     std::filesystem::remove_all(path_to_data_base_folder);
 }
+
+
+BOOST_AUTO_TEST_CASE(data_base_test_2)
+{
+    std::filesystem::path path_to_data_base_folder("local_test_base");
+
+    base::Bytes bytes1("sgfabvduflalfgfdnjknv  lcjnajfhvbadg ksd weufib34g 8vb");
+    base::Bytes bytes2("");
+    base::Bytes bytes3("empty _ key");
+    base::Bytes key1("test key");
+    base::Bytes key2("empty value");
+    base::Bytes key3("");
+
+    auto data_base = base::createClearDatabaseInstance(path_to_data_base_folder);
+
+    data_base.put(key1, bytes1);
+    data_base.put(key2, bytes2);
+    data_base.put(key3, bytes3);
+    BOOST_CHECK(data_base.exists(key1));
+    BOOST_CHECK(data_base.exists(key2));
+    BOOST_CHECK(data_base.exists(key3));
+
+    BOOST_CHECK_EQUAL(data_base.get(key1).value().toString(), bytes1.toString());
+    BOOST_CHECK_EQUAL(data_base.get(key2).value().toString(), bytes2.toString());
+    BOOST_CHECK_EQUAL(data_base.get(key3).value().toString(), bytes3.toString());
+
+    std::filesystem::remove_all(path_to_data_base_folder);
+}
+
+
+BOOST_AUTO_TEST_CASE(data_base_open)
+{
+    std::filesystem::path path_to_data_base_folder("local_test_base");
+
+    base::Bytes bytes1("sgfabvduflalfgfdnjknv  lcjnajfhvbadg ksd weufib34g 8vb");
+    base::Bytes bytes2("SGJ$( GDSN3tdgjs#)(u35");
+    base::Bytes key1("test key");
+    base::Bytes key2("test key too");
+    base::Bytes key3("I will remove it in the future");
+    {
+        auto data_base = base::createClearDatabaseInstance(path_to_data_base_folder);
+
+        data_base.put(key1, bytes1);
+        data_base.put(key2, bytes2);
+        data_base.put(key3, base::Bytes("rem"));
+        BOOST_CHECK(data_base.exists(key1));
+        BOOST_CHECK(data_base.exists(key2));
+        BOOST_CHECK(data_base.exists(key3));
+
+        data_base.remove(key3);
+        BOOST_CHECK(!data_base.exists(key3));
+    }
+
+    base::Database data_base2(path_to_data_base_folder);
+    BOOST_CHECK(data_base2.exists(key1));
+    BOOST_CHECK(data_base2.exists(key2));
+    BOOST_CHECK(!data_base2.exists(key3));
+
+    BOOST_CHECK_EQUAL(data_base2.get(key1).value().toString(), bytes1.toString());
+    BOOST_CHECK_EQUAL(data_base2.get(key2).value().toString(), bytes2.toString());
+
+    std::filesystem::remove_all(path_to_data_base_folder);
+}
