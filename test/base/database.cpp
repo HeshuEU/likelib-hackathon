dcc164179d312f184010b225ef1ed968fbf1dd9a
Oleg Malyshev <malyshev@heshu.by>
2019-12-20 17:45:56 +0300

    fixed the test name for database and added tests for property_tree

--- test/base/database.cpp
+++ test/base/database.cpp
@@ -33,7 +33,7 @@ BOOST_AUTO_TEST_CASE(data_base_test_1)
 }
 
 
-BOOST_AUTO_TEST_CASE(data_base_test_2)
+BOOST_AUTO_TEST_CASE(data_base_test_with_empty_key_and_value)
 {
     std::filesystem::path path_to_data_base_folder("local_test_base");
 
@@ -111,7 +111,7 @@ BOOST_AUTO_TEST_CASE(data_base_remove_not_exist_object)
 }
 
 
-BOOST_AUTO_TEST_CASE(data_base_open)
+BOOST_AUTO_TEST_CASE(data_base_constructor_from_file)
 {
     std::filesystem::path path_to_data_base_folder("local_test_base");
 
@@ -135,6 +135,42 @@ BOOST_AUTO_TEST_CASE(data_base_open)
     }
 
     base::Database data_base2(path_to_data_base_folder);
+    BOOST_CHECK(data_base2.exists(key1));
+    BOOST_CHECK(data_base2.exists(key2));
+    BOOST_CHECK(!data_base2.exists(key3));
+
+    BOOST_CHECK_EQUAL(data_base2.get(key1).value().toString(), bytes1.toString());
+    BOOST_CHECK_EQUAL(data_base2.get(key2).value().toString(), bytes2.toString());
+
+    std::filesystem::remove_all(path_to_data_base_folder);
+}
+
+
+BOOST_AUTO_TEST_CASE(data_base_open)
+{
+    std::filesystem::path path_to_data_base_folder("local_test_base");
+
+    base::Bytes bytes1("sgfabvduflalfgfdnjknv  lcjnajfhvbadg ksd weufib34g 8vb");
+    base::Bytes bytes2("SGJ$( GDSN3tdgjs#)(u35");
+    base::Bytes key1("test key");
+    base::Bytes key2("test key too");
+    base::Bytes key3("I will remove it in the future");
+    {
+        auto data_base = base::createClearDatabaseInstance(path_to_data_base_folder);
+
+        data_base.put(key1, bytes1);
+        data_base.put(key2, bytes2);
+        data_base.put(key3, base::Bytes("rem"));
+        BOOST_CHECK(data_base.exists(key1));
+        BOOST_CHECK(data_base.exists(key2));
+        BOOST_CHECK(data_base.exists(key3));
+
+        data_base.remove(key3);
+        BOOST_CHECK(!data_base.exists(key3));
+    }
+
+    base::Database data_base2;
+    data_base2.open(path_to_data_base_folder);
     BOOST_CHECK(data_base2.exists(key1));
     BOOST_CHECK(data_base2.exists(key2));
     BOOST_CHECK(!data_base2.exists(key3));
