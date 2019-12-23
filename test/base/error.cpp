e26e1b123e981a154235430adf9e7df093b251d7
Oleg Malyshev <malyshev@heshu.by>
2019-12-23 11:11:24 +0300

    added variables to the tests for Error

--- test/base/error.cpp
+++ test/base/error.cpp
@@ -6,56 +6,62 @@
 
 BOOST_AUTO_TEST_CASE(error_usage1)
 {
-    base::Error te("Test err");
-    BOOST_CHECK(te.toStdString() == "Test err");
-    BOOST_CHECK(std::string(te.what()) == "Test err");
+    std::string error_str = "Test err";
+    base::Error te(error_str);
+    BOOST_CHECK(te.toStdString() == error_str);
+    BOOST_CHECK(std::string(te.what()) == error_str);
 }
 
 
 BOOST_AUTO_TEST_CASE(error_constructor_copy)
 {
-    base::Error te("Test err copy");
+    std::string error_str = "Test err copy";
+    base::Error te(error_str);
     base::Error te2(te);
-    BOOST_CHECK(te.toStdString() == "Test err copy" && te.toStdString() == te2.toStdString());
-    BOOST_CHECK(std::string(te.what()) == "Test err copy" && std::string(te.what()) == std::string(te2.what()));
+    BOOST_CHECK(te.toStdString() == error_str && te.toStdString() == te2.toStdString());
+    BOOST_CHECK(std::string(te.what()) == error_str && std::string(te.what()) == std::string(te2.what()));
 }
 
 
 BOOST_AUTO_TEST_CASE(error_constructor_move)
 {
-    base::Error te("Test err move");
+    std::string error_str = "Test err move";
+    base::Error te(error_str);
     base::Error te2(std::move(te));
-    BOOST_CHECK(te2.toStdString() == "Test err move" && te.toStdString() == "");
-    BOOST_CHECK(std::string(te2.what()) == "Test err move" && std::string(te.what()) == "");
+    BOOST_CHECK(te2.toStdString() == error_str && te.toStdString() == "");
+    BOOST_CHECK(std::string(te2.what()) == error_str && std::string(te.what()) == "");
 }
 
 
 BOOST_AUTO_TEST_CASE(error_operator_equal)
 {
-    base::Error te("Test err");
+    std::string error_str = "Test err";
+    base::Error te(error_str);
     base::Error te2("Tes1 err");
     te2 = te;
-    BOOST_CHECK(te.toStdString() == "Test err" && te.toStdString() == te2.toStdString());
-    BOOST_CHECK(std::string(te.what()) == "Test err" && std::string(te.what()) == std::string(te2.what()));
+    BOOST_CHECK(te.toStdString() == error_str && te.toStdString() == te2.toStdString());
+    BOOST_CHECK(std::string(te.what()) == error_str && std::string(te.what()) == std::string(te2.what()));
 }
 
 
 BOOST_AUTO_TEST_CASE(error_operator_equal_move)
 {
-    base::Error te("Test err");
+    std::string error_str = "Test err";
+    base::Error te(error_str);
     base::Error te2("Tes1 err");
     te2 = std::move(te);
-    BOOST_CHECK(te2.toStdString() == "Test err" && te.toStdString() == "");
-    BOOST_CHECK(std::string(te2.what()) == "Test err" && std::string(te.what()) == "");
+    BOOST_CHECK(te2.toStdString() == error_str && te.toStdString() == "");
+    BOOST_CHECK(std::string(te2.what()) == error_str && std::string(te.what()) == "");
 }
 
 
 BOOST_AUTO_TEST_CASE(error_operator_output)
 {
-    base::Error te("1247468$^)9#2DZSF;LM gdsmTest err/N/N \n");
+    std::string error_str = "1247468$^)9#2DZSF;LM gdsmTest err/N/N \n";
+    base::Error te(error_str);
     std::stringstream stream;
     stream << te;
     std::string what;
     what = stream.str();
-    BOOST_CHECK(what == "1247468$^)9#2DZSF;LM gdsmTest err/N/N \n");
+    BOOST_CHECK(what == error_str);
 }
