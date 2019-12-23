9aa146814e7638fa419adc4f1a6f772709adf866
Oleg Malyshev <malyshev@heshu.by>
2019-12-23 11:17:59 +0300

    changed the tests for Error

--- test/base/error.cpp
+++ test/base/error.cpp
@@ -4,9 +4,18 @@
 
 #include <sstream>
 
-BOOST_AUTO_TEST_CASE(error_usage1)
+BOOST_AUTO_TEST_CASE(error_constructor)
 {
     std::string error_str = "Test err";
+    base::Error te(error_str);
+    BOOST_CHECK(te.toStdString() == error_str);
+    BOOST_CHECK(std::string(te.what()) == error_str);
+}
+
+
+BOOST_AUTO_TEST_CASE(error_constructor_with_empty_string)
+{
+    std::string error_str = "";
     base::Error te(error_str);
     BOOST_CHECK(te.toStdString() == error_str);
     BOOST_CHECK(std::string(te.what()) == error_str);
@@ -57,11 +66,15 @@ BOOST_AUTO_TEST_CASE(error_operator_equal_move)
 
 BOOST_AUTO_TEST_CASE(error_operator_output)
 {
-    std::string error_str = "1247468$^)9#2DZSF;LM gdsmTest err/N/N \n";
-    base::Error te(error_str);
+    std::string error_str1 = "1247468$^)9#2DZSF;LM gdsmTest err/N/N \n";
+    std::string error_str2 = "GFD #%*39F;LM gdsmTest err/N/N \n";
+    std::string error_str3 = "1247468$^)9#2DZSF;LMdsfj#%()630mk";
+    base::Error te1(error_str1);
+    base::Error te2(error_str2);
+    base::Error te3(error_str3);
+
     std::stringstream stream;
-    stream << te;
-    std::string what;
-    what = stream.str();
-    BOOST_CHECK(what == error_str);
+    std::string temp_str = "#)*fk%#0/n #%  sdf +_";
+    stream << te1 << temp_str << te2 << te3;
+    BOOST_CHECK(stream.str() == error_str1 + temp_str + error_str2 + error_str3);
 }
