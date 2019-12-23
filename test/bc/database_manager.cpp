a8bc47a2814fa120115d581419eca860609a4010
Oleg Malyshev <malyshev@heshu.by>
2019-12-23 18:21:34 +0300

    tests for DatabaseManager

--- test/bc/database_manager.cpp
+++ test/bc/database_manager.cpp
@@ -22,8 +22,18 @@ std::vector<bc::Block> getInitBlocks()
 
     bc::TransactionsSet set2;
     set2.add(bc::Transaction(bc::Address("from2"), bc::Address("to2"), bc::Balance(133), base::Time::now()));
+    set2.add(bc::Transaction(bc::Address("from3"), bc::Address("to3"), bc::Balance(85), base::Time()));
     blocks.push_back(bc::Block(hash3, set2));
     return blocks;
+}
+
+std::vector<bc::Transaction> getTransactionFromBlock(const bc::Block& block)
+{
+    std::vector<bc::Transaction> transactions;
+    for(const auto& transaction: block.getTransactions()) {
+        transactions.push_back(transaction);
+    }
+    return transactions;
 }
 } // namespace
 
@@ -39,7 +49,6 @@ BOOST_AUTO_TEST_CASE(database_manager_usage1)
 })";
     }
     base::PropertyTree config = base::readConfig("config.json");
-    BOOST_CHECK(config.hasKey("database.path"));
 
     bc::DatabaseManager manager(config);
 
@@ -52,6 +61,153 @@ BOOST_AUTO_TEST_CASE(database_manager_usage1)
     BOOST_CHECK(manager.findBlock(hash2) != std::nullopt);
     BOOST_CHECK(manager.findBlock(hash3) != std::nullopt);
     BOOST_CHECK(manager.getLastBlockHash() == hash3);
+
+    std::filesystem::remove_all("likelib/local_test_base");
+    std::filesystem::remove("config.json");
+}
+
+
+BOOST_AUTO_TEST_CASE(database_manager_usage2)
+{
+    {
+        std::ofstream output("config.json");
+        output << R"({
+    "database": {
+        "path": "likelib/local_test_base",
+        "clean": false
+    }
+})";
+    }
+    base::PropertyTree config = base::readConfig("config.json");
+
+    bc::DatabaseManager manager(config);
+
+    auto blocks = getInitBlocks();
+    manager.addBlock(blocks[0].getPrevBlockHash(), blocks[0]);
+    BOOST_CHECK(manager.findBlock(hash1) != std::nullopt);
+    BOOST_CHECK(manager.getLastBlockHash() == hash1);
+
+    manager.addBlock(blocks[1].getPrevBlockHash(), blocks[1]);
+    BOOST_CHECK(manager.findBlock(hash2) != std::nullopt);
+    BOOST_CHECK(manager.getLastBlockHash() == hash2);
+
+    manager.addBlock(blocks[2].getPrevBlockHash(), blocks[2]);
+    BOOST_CHECK(manager.findBlock(hash3) != std::nullopt);
+    BOOST_CHECK(manager.getLastBlockHash() == hash3);
+
+    std::filesystem::remove_all("likelib/local_test_base");
+    std::filesystem::remove("config.json");
+}
+
+
+BOOST_AUTO_TEST_CASE(database_manager_check_transaction_in_blocks)
+{
+    {
+        std::ofstream output("config.json");
+        output << R"({
+    "database": {
+        "path": "likelib/local_test_base",
+        "clean": false
+    }
+})";
+    }
+    base::PropertyTree config = base::readConfig("config.json");
+
+    bc::DatabaseManager manager(config);
+
+    auto blocks = getInitBlocks();
+    for(const auto& block: blocks) {
+        manager.addBlock(block.getPrevBlockHash(), block);
+    }
+
+    auto trans1 = getTransactionFromBlock(blocks[0]);
+    auto trans11 = getTransactionFromBlock(manager.findBlock(hash1).value()); // TODO:make it with for
+    bool res = true;
+    for(std::size_t i = 0; i < trans1.size(); i++) {
+        res = res && (trans1 == trans11);
+    }
+    BOOST_CHECK(res);
+
+    res = true;
+    auto trans2 = getTransactionFromBlock(blocks[1]);
+    auto trans22 = getTransactionFromBlock(manager.findBlock(hash2).value());
+    for(std::size_t i = 0; i < trans1.size(); i++) {
+        res = res && (trans2 == trans22);
+    }
+    BOOST_CHECK(res);
+
+    res = true;
+    auto trans3 = getTransactionFromBlock(blocks[2]);
+    auto trans33 = getTransactionFromBlock(manager.findBlock(hash3).value());
+    for(std::size_t i = 0; i < trans1.size(); i++) {
+        res = res && (trans3 == trans33);
+    }
+    BOOST_CHECK(res);
+
+    std::filesystem::remove_all("likelib/local_test_base");
+    std::filesystem::remove("config.json");
+}
+
+
+BOOST_AUTO_TEST_CASE(database_manager_with_not_empty_database)
+{
+    {
+        std::ofstream output("config.json");
+        output << R"({
+    "database": {
+        "path": "likelib/local_test_base",
+        "clean": false
+    }
+})";
+    }
+    base::PropertyTree config = base::readConfig("config.json");
+
+    {
+        bc::DatabaseManager manager(config);
+
+        auto blocks = getInitBlocks();
+        for(const auto& block: blocks) {
+            manager.addBlock(block.getPrevBlockHash(), block);
+        }
+    }
+
+    bc::DatabaseManager manager(config);
+    BOOST_CHECK(manager.findBlock(hash1) != std::nullopt);
+    BOOST_CHECK(manager.findBlock(hash2) != std::nullopt);
+    BOOST_CHECK(manager.findBlock(hash3) != std::nullopt);
+    BOOST_CHECK(manager.getLastBlockHash() == hash3);
+
+    std::filesystem::remove_all("likelib/local_test_base");
+    std::filesystem::remove("config.json");
+}
+
+
+BOOST_AUTO_TEST_CASE(database_manager_with_not_empty_database_and_clean)
+{
+    {
+        std::ofstream output("config.json");
+        output << R"({
+    "database": {
+        "path": "likelib/local_test_base",
+        "clean": true
+    }
+})";
+    }
+    base::PropertyTree config = base::readConfig("config.json");
+
+    {
+        bc::DatabaseManager manager(config);
+
+        auto blocks = getInitBlocks();
+        for(const auto& block: blocks) {
+            manager.addBlock(block.getPrevBlockHash(), block);
+        }
+    }
+
+    bc::DatabaseManager manager(config);
+    BOOST_CHECK(manager.findBlock(hash1) == std::nullopt);
+    BOOST_CHECK(manager.findBlock(hash2) == std::nullopt);
+    BOOST_CHECK(manager.findBlock(hash3) == std::nullopt);
 
     std::filesystem::remove_all("likelib/local_test_base");
     std::filesystem::remove("config.json");
