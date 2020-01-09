e58b84878ea6b3367f5ad6d88df9842a99fdd942
Daniel <daniel@heshu.by>
2020-01-09 12:49:25 +0300

    fixed wrong merge

--- src/base/bytes.cpp
+++ src/base/bytes.cpp
@@ -1,6 +1,7 @@
 #include "bytes.hpp"
 
 #include "base/assert.hpp"
+#include "base/error.hpp"
 
 #include <boost/container_hash/hash.hpp>
 
@@ -219,6 +220,46 @@ std::ostream& operator<<(std::ostream& os, const Bytes& bytes)
     return os << bytes.toHex();
 }
 
+
+namespace
+{
+
+    std::size_t hexToInt(char hex)
+    {
+        if('0' <= hex && hex <= '9') {
+            return hex - '0';
+        }
+        else if('a' <= hex && hex <= 'f') {
+            return hex - 'a' + 10;
+        }
+        else if('A' <= hex && hex <= 'F') {
+            return hex - 'A' + 10;
+        }
+        else {
+            RAISE_ERROR(base::InvalidArgument, "Non hex symbol.");
+        }
+    }
+
+} // namespace
+
+
+Bytes Bytes::fromHex(const std::string_view& hex_view)
+{
+    if(hex_view.size() % 2 != 0) {
+        RAISE_ERROR(InvalidArgument, "Invalid string length. Odd line length.");
+    }
+
+    auto bytes_size = hex_view.size() / 2;
+    std::vector<Byte> bytes(bytes_size);
+    for(std::size_t current_symbol_index = 0; current_symbol_index < bytes_size; current_symbol_index++) {
+        auto index = current_symbol_index * 2;
+        auto high_part = hexToInt(hex_view[index]);
+        auto low_part = hexToInt(hex_view[index + 1]);
+        bytes[current_symbol_index] = (high_part << 4) + low_part;
+    }
+
+    return Bytes(bytes);
+}
 
 } // namespace base
 
