c976c79491fcf1669b767a68783bd9cda618771f
Daniel <daniel@heshu.by>
2020-01-22 17:49:45 +0300

    removed wrong paths searching in integration tests framework

--- test/integration_test/run_test.py
+++ test/integration_test/run_test.py
@@ -12,20 +12,10 @@ import test_multi_transfer
 # TODO: make script multi-platform
 
 if __name__ == "__main__":
-    candiadte_folders = glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "*build*/"), recursive=False)
 
-    if len(candiadte_folders) == 0:
-        node_exec_build_relative_path = "node"
-        rpc_client_exec_build_relative_path = "rpc-client"
-    elif len(candiadte_folders) == 1:
-        build_dir = candiadte_folders[0]
-        node_exec_build_relative_path = os.path.join(
-            build_dir, "src", "node", "node")
-        rpc_client_exec_build_relative_path = os.path.join(
-            build_dir, "src", "rpc-client", "rpc-client")
-    else:
-        print("Need to specify paths to test execute files. Run \"python3 run_test.py --help\" to read more.")
-        exit(2)
+    node_exec_build_relative_path = "node"
+    rpc_client_exec_build_relative_path = "rpc-client"
+
 
     parser = argparse.ArgumentParser(description='Test execution params')
 
