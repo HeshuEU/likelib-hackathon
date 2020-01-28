dd81ad90bdc1c9e0f8d23afa534d2c5fd6eae0a7
Oleg Malyshev <malyshev@heshu.by>
2020-01-28 18:13:08 +0300

    fixed the bug that didn't close nodes

--- test/integration_test/test_multi_transfer.py
+++ test/integration_test/test_multi_transfer.py
@@ -2,27 +2,6 @@ from tester import NodeRunner, NodeId, Client, TEST_CHECK, test_case
 import threading
 
 
-def check_test_received(log_line):
-    if "Node received in {test}" in log_line:
-        return True
-    else:
-        return False
-
-
-def check_connect_asepted(log_line):
-    if "Connection accepted:" in log_line:
-        return True
-    else:
-        return False
-
-def check_connection_established_builder(node_info):
-    def check_connect_established(log_line):
-        if f"Connection established: {node_info.connect_sync_address}" in log_line:
-            return True
-        else:
-            return False
-    return check_connect_established
-
 def check_transaction_check_builder(from_address, to_address, amount):
     def check_transaction_receives(log_line):
         if " Node received in {transaction}: from_address["+from_address+"], to_address["+to_address+"], amount["+str(amount)+"]" in log_line:
@@ -52,7 +31,6 @@ def init_nodes_with_everything(node_exec_path, client, nodes, nodes_id, count_no
     nodes.append(NodeRunner(node_exec_path, NodeRunner.generate_config(current_node_id = nodes_id[0]), "node_1", start_up_time = waiting_time))
     nodes[0].start()
     TEST_CHECK(client.run_check_test(host_id = nodes_id[0]))
-    TEST_CHECK(nodes[0].check(check_test_received))
 
     for i in range(1, count_nodes):
         node_info = NodeId(sync_port=start_sync_port + i, rpc_port=start_rpc_port + i, absolute_address = "127.0.0.1")
@@ -71,7 +49,6 @@ def init_nodes_one_by_one(node_exec_path, client, nodes, nodes_id, count_nodes):
     nodes.append(NodeRunner(node_exec_path, NodeRunner.generate_config(current_node_id = nodes_id[0]), "node_1", start_up_time = waiting_time))
     nodes[0].start()
     TEST_CHECK(client.run_check_test(host_id = nodes_id[0]))
-    TEST_CHECK(nodes[0].check(check_test_received))
 
     for i in range(1, count_nodes):
         nodes_id.append(NodeId(sync_port=start_sync_port + i, rpc_port=start_rpc_port + i, absolute_address = "127.0.0.1"))
@@ -81,10 +58,6 @@ def init_nodes_one_by_one(node_exec_path, client, nodes, nodes_id, count_nodes):
 
         for j in range(i + 1):
             TEST_CHECK(client.run_check_test(host_id=nodes_id[j]))
-        
-        TEST_CHECK(nodes[i].check(check_test_received))
-        TEST_CHECK(nodes[i - 1].check(check_connect_asepted))
-        TEST_CHECK(nodes[i].check(check_connection_established_builder(nodes_id[i - 1])))
 
 
 def init_transfers(client, nodes_id, addresses, amount, transaction_wait = 2):
@@ -100,6 +73,10 @@ def init_transfers(client, nodes_id, addresses, amount, transaction_wait = 2):
         for j in nodes_id:
             TEST_CHECK(client.run_check_balance(address=to_address, host_id=j, target_balance=amount))
 
+
+def close_nodes(nodes):
+    for node in nodes:
+        node.close()
 
 
 def node_transfers(node_id, client, addresses):
@@ -125,37 +102,33 @@ def main(node_exec_path, rpc_client_exec_path):
     node_id_1 = NodeId(sync_port=20206, rpc_port=50056)
     node_id_2 = NodeId(sync_port=20202, rpc_port=50053)
 
-    client = Client(rpc_client_exec_path, "client")
+    try:
+        client = Client(rpc_client_exec_path, "client")
 
-    with NodeRunner(node_exec_path, NodeRunner.generate_config(current_node_id=node_id_1), "node_1") as node_1:
-
-        TEST_CHECK(client.run_check_test(host_id=node_id_1))
-        TEST_CHECK(node_1.check(check_test_received))
-
-        with NodeRunner(node_exec_path, NodeRunner.generate_config(current_node_id=node_id_2, nodes_id_list=[node_id_1, ]), "node_2") as node_2:
+        with NodeRunner(node_exec_path, NodeRunner.generate_config(current_node_id=node_id_1), "node_1") as node_1:
 
             TEST_CHECK(client.run_check_test(host_id=node_id_1))
-            TEST_CHECK(client.run_check_test(host_id=node_id_2))
-            TEST_CHECK(node_2.check(check_test_received))
 
-            TEST_CHECK(node_1.check(check_connect_asepted))
-            TEST_CHECK(node_2.check(check_connection_established_builder(node_id_1)))
+            with NodeRunner(node_exec_path, NodeRunner.generate_config(current_node_id=node_id_2, nodes_id_list=[node_id_1, ]), "node_2") as node_2:
 
-            target_address = "1" * 32
+                target_address = "1" * 32
 
-            TEST_CHECK(client.run_check_balance(address=target_address, host_id=node_id_2, target_balance=0))
+                TEST_CHECK(client.run_check_balance(address=target_address, host_id=node_id_2, target_balance=0))
 
-            from_address = "0" * 32
-            amount = 333
-            transaction_wait = 2
-            TEST_CHECK(client.run_check_transfer(from_address=from_address, host_id=node_id_2, to_address=target_address, amount=amount, wait=transaction_wait))
+                from_address = "0" * 32
+                amount = 333
+                transaction_wait = 2
+                TEST_CHECK(client.run_check_transfer(from_address=from_address, host_id=node_id_2, to_address=target_address, amount=amount, wait=transaction_wait))
 
-            TEST_CHECK(node_2.check(check_transaction_check_builder(from_address, target_address, amount)))
-            TEST_CHECK(node_2.check(check_block_add))
-            TEST_CHECK(client.run_check_balance(address=target_address, host_id=node_id_2, target_balance=amount))
-            TEST_CHECK(node_1.check(check_block_add))
-            TEST_CHECK(client.run_check_balance(address=target_address, host_id=node_id_1, target_balance=amount))
-
+                TEST_CHECK(node_2.check(check_transaction_check_builder(from_address, target_address, amount)))
+                TEST_CHECK(node_2.check(check_block_add))
+                TEST_CHECK(client.run_check_balance(address=target_address, host_id=node_id_2, target_balance=amount))
+                TEST_CHECK(node_1.check(check_block_add))
+                TEST_CHECK(client.run_check_balance(address=target_address, host_id=node_id_1, target_balance=amount))
+    except Exception as exs:
+        print(exs)
+        return 1
+        
     return 0
 
 
@@ -166,35 +139,34 @@ def main(node_exec_path, rpc_client_exec_path):
     client = Client(rpc_client_exec_path, "client")
     nodes_id = []
     nodes = []
-    init_nodes_with_everything(node_exec_path, client, nodes, nodes_id, count_nodes)
+    try:
+        init_nodes_with_everything(node_exec_path, client, nodes, nodes_id, count_nodes)
 
-    for i in range(1, count_nodes):
-        for j in range(i + 1):
-                TEST_CHECK(client.run_check_test(host_id=nodes_id[j]))
-            
-        TEST_CHECK(nodes[i].check(check_test_received))
-        TEST_CHECK(nodes[i - 1].check(check_connect_asepted))
-        for j in range(i):
-            TEST_CHECK(nodes[i].check(check_connection_established_builder(nodes_id[j])))
+        for i in range(1, count_nodes):
+            for j in range(i + 1):
+                    TEST_CHECK(client.run_check_test(host_id=nodes_id[j]))
 
-    init_amount = 1000
-    addresses = init_addresses(count_nodes)
-    init_transfers(client, nodes_id, addresses, init_amount)
-    
-    for i in range(1, len(addresses) - 1):
-        from_address = addresses[i]
-        to_address = addresses[i + 1]
-        amount = i * 100
-        transaction_wait = 4
-        TEST_CHECK(client.run_check_transfer(from_address=from_address, host_id=nodes_id[i], to_address=to_address, amount=amount, wait=transaction_wait))
+        init_amount = 1000
+        addresses = init_addresses(count_nodes)
+        init_transfers(client, nodes_id, addresses, init_amount)
+        
+        for i in range(1, len(addresses) - 1):
+            from_address = addresses[i]
+            to_address = addresses[i + 1]
+            amount = i * 100
+            transaction_wait = 4
+            TEST_CHECK(client.run_check_transfer(from_address=from_address, host_id=nodes_id[i], to_address=to_address, amount=amount, wait=transaction_wait))
 
-        for j in nodes_id:
-            TEST_CHECK(client.run_check_balance(address=to_address, host_id=j, target_balance = init_amount + amount))
-            TEST_CHECK(client.run_check_balance(address=from_address, host_id=j, target_balance = init_amount - amount  + (i - 1) * 100))
+            for j in nodes_id:
+                TEST_CHECK(client.run_check_balance(address=to_address, host_id=j, target_balance = init_amount + amount))
+                TEST_CHECK(client.run_check_balance(address=from_address, host_id=j, target_balance = init_amount - amount  + (i - 1) * 100))
 
-
-    for node in nodes:
-        node.close()
+    except Exception as exs:
+        print(exs)
+        close_nodes(nodes)
+        return 1
+        
+    close_nodes(nodes)
     return 0
 
 
@@ -205,26 +177,30 @@ def main(node_exec_path, rpc_client_exec_path):
     client = Client(rpc_client_exec_path, "client")
     nodes_id = []
     nodes = []
-    init_nodes_one_by_one(node_exec_path, client, nodes, nodes_id, count_nodes)
+    try:
+        init_nodes_one_by_one(node_exec_path, client, nodes, nodes_id, count_nodes)
 
-    init_amount = 1000
-    addresses = init_addresses(count_nodes)
-    init_transfers(client, nodes_id, addresses, init_amount)
+        init_amount = 1000
+        addresses = init_addresses(count_nodes)
+        init_transfers(client, nodes_id, addresses, init_amount)
 
-    for i in range(1, len(addresses) - 1):
-        from_address = addresses[i]
-        to_address = addresses[i + 1]
-        amount = i * 100
-        transaction_wait = 4
-        TEST_CHECK(client.run_check_transfer(from_address=from_address, host_id=nodes_id[i], to_address=to_address, amount=amount, wait=transaction_wait))
+        for i in range(1, len(addresses) - 1):
+            from_address = addresses[i]
+            to_address = addresses[i + 1]
+            amount = i * 100
+            transaction_wait = 4
+            TEST_CHECK(client.run_check_transfer(from_address=from_address, host_id=nodes_id[i], to_address=to_address, amount=amount, wait=transaction_wait))
 
-        for j in nodes_id:
-            TEST_CHECK(client.run_check_balance(address=to_address, host_id=j, target_balance = init_amount + amount))
-            TEST_CHECK(client.run_check_balance(address=from_address, host_id=j, target_balance = init_amount - amount  + (i - 1) * 100))
+            for j in nodes_id:
+                TEST_CHECK(client.run_check_balance(address=to_address, host_id=j, target_balance = init_amount + amount))
+                TEST_CHECK(client.run_check_balance(address=from_address, host_id=j, target_balance = init_amount - amount  + (i - 1) * 100))
 
-
-    for node in nodes:
-        node.close()
+    except Exception as exs:
+        print(exs)
+        close_nodes(nodes)
+        return 1
+        
+    close_nodes(nodes)
     return 0
 
 
@@ -235,25 +211,30 @@ def main(node_exec_path, rpc_client_exec_path):
     client = Client(rpc_client_exec_path, "client")
     nodes_id = []
     nodes = []
-    init_nodes_with_everything(node_exec_path, client, nodes, nodes_id, count_nodes)
+    try:
+        init_nodes_with_everything(node_exec_path, client, nodes, nodes_id, count_nodes)
 
-    init_amount = 1000
-    address_per_nodes = 2
-    addresses = (init_addresses(count_nodes * address_per_nodes + 1))
-    init_transfers(client, nodes_id, addresses, init_amount)
+        init_amount = 1000
+        address_per_nodes = 2
+        addresses = (init_addresses(count_nodes * address_per_nodes + 1))
+        init_transfers(client, nodes_id, addresses, init_amount)
 
-    threads = []
-    for i in range(len(nodes_id)):
-        from_address = i * address_per_nodes + 1
-        to_address = i * address_per_nodes + +address_per_nodes + 1
-        threads.append(threading.Thread(target=node_transfers, args=(nodes_id[i], client, addresses[from_address : to_address])))
-        threads[i].start()
+        threads = []
+        for i in range(len(nodes_id)):
+            from_address = i * address_per_nodes + 1
+            to_address = i * address_per_nodes + +address_per_nodes + 1
+            threads.append(threading.Thread(target=node_transfers, args=(nodes_id[i], client, addresses[from_address : to_address])))
+            threads[i].start()
 
-    for thread in threads:
-         thread.join(150) 
-    for i in range(1, len(addresses)):
-        for node_id in nodes_id:
-            TEST_CHECK(client.run_check_balance(address=addresses[i], host_id=node_id, target_balance = init_amount))
-    for node in nodes:
-        node.close()
+        for thread in threads:
+            thread.join(150) 
+        for i in range(1, len(addresses)):
+            for node_id in nodes_id:
+                TEST_CHECK(client.run_check_balance(address=addresses[i], host_id=node_id, target_balance = init_amount))
+    except Exception as exs:
+        print(exs)
+        close_nodes(nodes)
+        return 1
+        
+    close_nodes(nodes)
     return 0
