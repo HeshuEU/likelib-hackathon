0a85828d1026dd42faff57159cfe764d003f284c
siarhei.sadouski <sadovski@heshu.by>
2020-02-17 13:14:22 +0300

    Fix 4

--- doc/prepare_build.sh
+++ doc/prepare_build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if apt-get update; then
+if apt-get update > /dev/null; then
    SUDO_PREF=""
 else
    SUDO_PREF=sudo
 fi
 
 ${SUDO_PREF} apt-get update || exit 1
-${SUDO_PREF} apt-get install apt-utils || exit 1
+${SUDO_PREF} apt-get install apt-utils -y || exit 1
+${SUDO_PREF} apt-get update || exit 1
 ${SUDO_PREF} apt-get dist-upgrade -y || exit 1
+${SUDO_PREF} apt-get update || exit 1
 
 ${SUDO_PREF} apt-get install -y build-essential gcc g++ make cmake || exit 1
 
@@ -17,9 +19,9 @@ ${SUDO_PREF} apt-get install -y git wget unzip tar curl valgrind clang-tidy pyth
 ${SUDO_PREF} apt-get install -y  software-properties-common || exit 1
 ${SUDO_PREF} apt-get update || exit 1
 
-${SUDO_PREF} add-apt-repository ppa:ethereum/ethereum || exit 1
+${SUDO_PREF} add-apt-repository ppa:ethereum/ethereum -y || exit 1
 ${SUDO_PREF} apt-get update || exit 1
-${SUDO_PREF} apt-get install solc || exit 1
+${SUDO_PREF} apt-get install -y solc || exit 1
 
 pip3 install conan || exit 1
 conan remote remove conan-center || exit 1
