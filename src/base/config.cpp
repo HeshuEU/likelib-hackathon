583e303d6d0ff0806af12b37f9090d9e2393be6b
Daniel <daniel@heshu.by>
2020-02-19 13:52:03 +0300

    added config.cpp

---
+++ src/base/config.cpp
@@ -0,0 +1,26 @@
+#include "config.hpp"
+
+
+namespace
+{
+constexpr const char* const KEY_FILE_PREFIX = "lkkey";
+}
+
+
+namespace base::config
+{
+
+std::filesystem::path makePublicKeyPath(const std::filesystem::path& directory_path)
+{
+    auto ret = directory_path / KEY_FILE_PREFIX;
+    ret.replace_extension(".pub");
+    return ret;
+}
+
+
+std::filesystem::path makePrivateKeyPath(const std::filesystem::path& directory_path)
+{
+    return directory_path / KEY_FILE_PREFIX;
+}
+
+} // namespace base::config
