664e8dcc3c09bd64b05ea0de5461c657ef400f92
Daniel <daniel@heshu.by>
2020-03-02 18:12:35 +0300

    unstable: began to refactor client actions

--- src/client/actions.hpp
+++ src/client/actions.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-#include "base/subprogram_router.hpp"
+#include <base/config.hpp>
+#include <base/subprogram_router.hpp>
+
 
 int testConnection(base::SubprogramRouter& router);
 int getBalance(base::SubprogramRouter& router);
@@ -9,3 +11,28 @@ int createContract(base::SubprogramRouter& router);
 int messageCall(base::SubprogramRouter& router);
 int compileCode(base::SubprogramRouter& router);
 int generateKeys(base::SubprogramRouter& router);
+
+
+class ActionBase {
+public:
+    explicit ActionBase(base::SubprogramRouter& router)
+        : _router{router}
+    {}
+
+    virtual ~ActionBase() = default;
+
+    virtual void setupOptionsParser(base::ProgramOptionsParser& parser) const = 0;
+    virtual void checkOptions(const base::ProgramOptionsParser& parser) const = 0;
+    virtual void execute() const = 0;
+
+    int run() {
+        setupOptionsParser(_router.getOptionsParser());
+        checkOptions(_router.getOptionsParser());
+        execute();
+
+        return base::config::EXIT_OK;
+    }
+
+private:
+    base::SubprogramRouter& _router;
+};
