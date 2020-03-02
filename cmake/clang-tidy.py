0ce668b3f7864637d2b77885862cd1cc3172c737
Belski Dzianis <belski@heshu.by>
2020-03-02 18:33:10 +0300

    not using code comented

--- cmake/clang-tidy.py
+++ cmake/clang-tidy.py
@@ -5,9 +5,9 @@ import sys, subprocess
 PATH_TO_LOGS="/tmp/logs/"
 LOG_NAME="clang-tidy-output.txt"
 #print(len(sys.argv))
-if not "-isystem" in sys.argv:
-    sys.argv.append("-isystem")
-index = sys.argv.index("-isystem")
+#if not "-isystem" in sys.argv:
+#    sys.argv.append("-isystem")
+#index = sys.argv.index("-isystem")
 l = sys.argv
 #l = l[:index + 1] + ["/usr/lib/gcc/x86_64-linux-gnu/9/include/", "-isystem"] + l[index + 1: ]
 l[0] = "/usr/bin/clang-tidy"
