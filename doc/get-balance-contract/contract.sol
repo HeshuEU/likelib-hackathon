584a2d6a2d1f72f5b45e4824d7b578aab4ff7ee8
Oleg Malyshev <malyshev@heshu.by>
2020-03-20 13:32:16 +0300

    write get_balance Solidity contract

---
+++ doc/get-balance-contract/contract.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+contract Balance {
+    address public stored_address;
+    
+    function setAddress() public{
+        stored_address = msg.sender;
+    }
+
+    function getBalance() public view returns (uint256){
+        return stored_address.balance;
+    }
+}
