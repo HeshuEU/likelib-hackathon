ecb7ba25bbe04c5dcddaf023b7dc5dc102541792
siarhei.sadouski <sadovski@heshu.by>
2020-03-23 12:40:18 +0300

    Working contract processing at integration tests

--- test/integration_test/run_test.py
+++ test/integration_test/run_test.py
@@ -1,41 +1,21 @@
-import os
 import argparse
 
 import tester
 
 import test_connection
 import test_transfer
+import test_contract
+
 # import test_multi_network
 # import test_multi_transfer
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test execution params')
-    parser.add_argument('-c', '--client', type=str, default="client",
-                        help='client execution file path')
-    parser.add_argument('-n', '--node', type=str, default="node",
-                        help='node execution file path')
+    parser.add_argument('-b', '--bin', type=str, help='path to folder with dependencies')
     parser.add_argument('-t', '--tests', type=str, default="",
                         help='pattern for test execution')
-    parser.add_argument('-e', '--evm', type=str, default="libevmone.so.0.4",
-                        help='evm execution file path')
 
     args = parser.parse_args()
 
-    if not os.path.exists(args.node):
-        print(f"Not found node executalble file by path: {args.node}")
-        exit(2)
-
-    if not os.path.exists(args.client):
-        print(
-            f"Not found rpc client executalble file by path: {args.client}")
-        exit(2)
-
-    if not os.path.exists(args.evm):
-        print(
-            f"Not found evm shared library file by path: {args.evm}")
-        exit(2)
-
-    return_code = tester.run_registered_test_cases(
-        args.tests, os.path.abspath(args.node), os.path.abspath(args.client), os.path.abspath(args.evm))
+    return_code = tester.run_registered_test_cases(args.tests, args.bin)
     exit(return_code)
--- test/integration_test/test_connection.py
+++ test/integration_test/test_connection.py
@@ -2,9 +2,8 @@ from tester import test_case, Node
 
 
 @test_case("connection")
-def main(logger, node_exec_path, rpc_client_exec_path, evm_path):
-    node_settings = Node.Settings(
-        node_exec_path, rpc_client_exec_path, evm_path, Node.Id(20205, 50055))
-    with Node(node_settings, logger) as node:
+def main(env, logger):
+    node_settings = Node.Settings(Node.Id(20205, 50055))
+    with Node(env, node_settings, logger) as node:
         node.run_check_test()
     return 0
