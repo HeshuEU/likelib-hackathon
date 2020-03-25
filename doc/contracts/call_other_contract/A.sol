b7e4fd9f2e1810bffcc894324eafe0ede31fb124
Siarhei Sadouski <sadovski@heshu.by>
2020-03-25 13:47:28 +0300

    Add address parser

--- doc/contracts/call_other_contract/A.sol
+++ doc/contracts/call_other_contract/A.sol
@@ -1,10 +1,9 @@
 pragma solidity >=0.4.0 <0.7.0;
 
-import "AbstractA.sol";
+import "./AbstractA.sol";
 
 contract A is AbstractA {
-    // implementation of f1
-    function sum(uint arg1, uint arg2) returns(uint) {
+    function sum(uint arg1, uint arg2) external override returns(uint) {
         return arg1 + arg2;
     }
 }
