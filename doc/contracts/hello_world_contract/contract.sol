41139f946716b42496d09b5b2bb8cec2dc8bb4f3
siarhei.sadouski <sadovski@heshu.by>
2020-03-26 10:19:20 +0300

    formatting

--- doc/contracts/hello_world_contract/contract.sol
+++ doc/contracts/hello_world_contract/contract.sol
@@ -3,18 +3,18 @@ pragma solidity >=0.4.0 <0.7.0;
 contract Hello {
 
     string defaultName;
-    mapping (address => string) public accounts;
+    mapping(address => string) public accounts;
 
     constructor() public {
         defaultName = 'World';
     }
 
-    function getMessage() public view returns(string memory) {
+    function getMessage() public view returns (string memory) {
         string memory name = bytes(accounts[msg.sender]).length > 0 ? accounts[msg.sender] : defaultName;
-        return concat("Hello, " , name);
+        return concat("Hello, ", name);
     }
 
-    function setName(string memory name) public returns(bool success) {
+    function setName(string memory name) public returns (bool success) {
         require(bytes(name).length > 0);
         accounts[msg.sender] = name;
         return true;
@@ -28,14 +28,15 @@ contract Hello {
         bytes memory _newValue = bytes(_tmpValue);
 
         uint j = 0;
-        for(uint i = 0; i < _baseBytes.length; i++) {
+        for (uint i = 0; i < _baseBytes.length; i++) {
             _newValue[j++] = _baseBytes[i];
         }
 
-        for(uint i = 0; i < _valueBytes.length; i++) {
+        for (uint i = 0; i < _valueBytes.length; i++) {
             _newValue[j++] = _valueBytes[i];
         }
 
         return string(_newValue);
     }
+
 }
