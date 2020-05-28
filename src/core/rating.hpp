0a97325586e563fa4a4ed4b93401f74750d6a1b3
Daniil Zhuravlev <zhuravlev@heshu.by>
2020-05-28 15:15:43 +0300

    added rating

---
+++ src/core/rating.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "base/bytes.hpp"
+#include "base/config.hpp"
+#include "base/database.hpp"
+#include "base/property_tree.hpp"
+#include "base/serialization.hpp"
+#include "base/time.hpp"
+#include "net/endpoint.hpp"
+
+#include <cstdint>
+#include <type_traits>
+
+namespace lk
+{
+
+class Rating
+{
+  public:
+    using Value = std::int16_t;
+
+    Rating(const net::Endpoint& ep, base::Database& db);
+
+    Value getValue();
+
+    explicit operator bool() const noexcept; // if false, then the peer is too bad
+    bool isGood() const noexcept;
+
+    Rating& nonExpectedMessage() noexcept;
+    Rating& invalidMessage() noexcept;
+    Rating& badBlock() noexcept;
+    Rating& differentGenesis() noexcept;
+    Rating& connectionRefused() noexcept;
+
+  private:
+    static constexpr Value INITIAL_PEER_RATING = 20;
+
+    const base::Bytes _serialized_ep;
+    base::Database& _db;
+
+    struct Data
+    {
+        Value value;
+        base::Time ts;
+
+        void serialize(base::SerializationOArchive& oa) const;
+        static Data deserialize(base::SerializationIArchive& ia);
+    };
+    Data _data;
+
+    void dbUpdate();
+};
+
+
+class RatingManager
+{
+  public:
+    RatingManager(const base::PropertyTree& config);
+
+    Rating get(const net::Endpoint& ep);
+  private:
+    base::Database _db;
+};
+
+}
