97860c714a2dd562c6b8095b7824d13355461188
Dzianis Belski <belski@heshu.by>
2020-05-28 19:04:17 +0300

    Update prepare_build.sh

--- doc/prepare_build.sh
+++ doc/prepare_build.sh
@@ -22,6 +22,7 @@ ${SUDO_PREF} apt-get install -y git wget unzip tar curl valgrind \
                                 clang-tidy python3.7 python3-pip \
                                 solc autoconf libtool || exit 1
 pip3 install web3 || exit 1
+pip3 install coincurve || exit 1
 
 #cmake --version || (${SUDO_PREF} apt-get install -y build-essential gcc g++ make cmake || exit 1)
 if ! command -v cmake; then
