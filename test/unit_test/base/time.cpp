f7cee2480e2e5d52c507bc5f12837a1ca392eb81
Daniil Zhuravlev <zhuravlev@heshu.by>
2020-05-28 15:17:08 +0300

    unit tests for timer fixed

--- test/unit_test/base/time.cpp
+++ test/unit_test/base/time.cpp
@@ -7,8 +7,8 @@ BOOST_AUTO_TEST_CASE(time_default_constructor)
     base::Time time1;
     base::Time time2;
     BOOST_CHECK(time1 == time2);
-    BOOST_CHECK(time1.getSecondsSinceEpoch() == 0);
-    BOOST_CHECK(time2.getSecondsSinceEpoch() == 0);
+    BOOST_CHECK(time1.getSeconds() == 0);
+    BOOST_CHECK(time2.getSeconds() == 0);
 }
 
 
@@ -16,7 +16,7 @@ BOOST_AUTO_TEST_CASE(time_from_TimePoint1)
 {
     base::Time time1{ base::Time::now() };
     auto time2 = base::Time(std::chrono::system_clock::now());
-    BOOST_CHECK(time1.getSecondsSinceEpoch() == time2.getSecondsSinceEpoch());
+    BOOST_CHECK(time1.getSeconds() == time2.getSeconds());
     BOOST_CHECK(time1.toTimePoint() == time2.toTimePoint());
     BOOST_CHECK(time1 == time2);
 }
@@ -26,7 +26,7 @@ BOOST_AUTO_TEST_CASE(time_from_TimePoint2)
 {
     base::Time time1{ base::Time::now() };
     base::Time time2{ base::Time(time1.toTimePoint()) };
-    BOOST_CHECK(time1.getSecondsSinceEpoch() == time2.getSecondsSinceEpoch());
+    BOOST_CHECK(time1.getSeconds() == time2.getSeconds());
     BOOST_CHECK(time1.toTimePoint() == time2.toTimePoint());
     BOOST_CHECK(time1 == time2);
 }
@@ -40,17 +40,17 @@ BOOST_AUTO_TEST_CASE(time_operator_equal)
     time2 = time1;
     BOOST_CHECK(time1 == time2);
     time2 = base::Time::now();
-    BOOST_CHECK(time1.getSecondsSinceEpoch() == time2.getSecondsSinceEpoch());
+    BOOST_CHECK(time1.getSeconds() == time2.getSeconds());
 }
 
 
 BOOST_AUTO_TEST_CASE(time_from_seconds)
 {
     base::Time time1(base::Time::now());
-    auto seconds = time1.getSecondsSinceEpoch();
+    auto seconds = time1.getSeconds();
     base::Time time2(seconds);
     BOOST_CHECK(time1 == time2);
-    BOOST_CHECK(time1.getSecondsSinceEpoch() == time2.getSecondsSinceEpoch());
+    BOOST_CHECK(time1.getSeconds() == time2.getSeconds());
 }
 
 
@@ -79,9 +79,9 @@ BOOST_AUTO_TEST_CASE(time_serialization2)
 BOOST_AUTO_TEST_CASE(time_seconds_serialization)
 {
     auto now{ base::Time::now() };
-    auto time_1_serialized{ now.getSecondsSinceEpoch() };
+    auto time_1_serialized{ now.getSeconds() };
     auto now_2{ base::Time(time_1_serialized) };
-    auto time_2_serialized{ now_2.getSecondsSinceEpoch() };
+    auto time_2_serialized{ now_2.getSeconds() };
     BOOST_CHECK_EQUAL(time_1_serialized, time_2_serialized);
 }
 
@@ -91,6 +91,6 @@ BOOST_AUTO_TEST_CASE(time_toBytes_from_Bytes)
     auto time1{ base::Time::now() };
     auto b = base::toBytes(time1);
     auto time2 = base::fromBytes<base::Time>(b);
-    BOOST_CHECK(time1.getSecondsSinceEpoch() == time2.getSecondsSinceEpoch());
+    BOOST_CHECK(time1.getSeconds() == time2.getSeconds());
     BOOST_CHECK(time1.toTimePoint() == time2.toTimePoint());
 }
