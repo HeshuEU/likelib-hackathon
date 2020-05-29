a67163d67f89149292f55564259131deaaded438
Belski Dzianis <belski@heshu.by>
2020-05-29 12:43:37 +0300

    clang-tidy output fixed

--- cmake/clang-tidy.py
+++ cmake/clang-tidy.py
@@ -21,7 +21,8 @@ try:
         f.write('\n'.join(output))
 except Exception as e:
     with open(os.path.join(PATH_TO_LOGS, LOG_NAME), "at", encoding='utf8') as f:
-        f.write(str(e))
+        # Print output with correct \n
+        f.write(str(e.output.decode("utf-8")))
     open(os.path.join(PATH_TO_LOGS, ".error-flag"), "w").close()
     # Set error flag, and continue check other files with exit(0)
     exit(0)
