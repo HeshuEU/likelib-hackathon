2844ba07ca63e093d26b1fc7110fd78a4d2eba80
Dzianis Belski <belski@heshu.by>
2020-06-22 10:24:47 +0300

    double connection in bad network added

--- test/integration_test/test_bad_network.py
+++ test/integration_test/test_bad_network.py
@@ -9,18 +9,7 @@ def log_subprocess_output(pipe, env):
     for line in iter(pipe.readline, b''): # b'\n'-separated lines
       env.logger.info(f"start_bad_network.sh: {line}")
 
-@test_case("connect_node_to_bad_network")
-def main(env: Env) -> int:
-    sync_port = 20100
-    grpc_port = 50100
-    amount = randrange(1000)
-    update_time = 0.5
-    transaction_update_time = 2
-    max_update_request = 10
-    timeout = 2
-    wait_time = 1
-
-    env.logger.debug(f"Random amount for test = {amount}")
+def run_bad_nodes(env):
     script_path = os.path.realpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                       "..", "..", "test", "integration_test", "tester", "start_bad_network.sh"))
     process=subprocess.Popen([f"{script_path}"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -39,11 +28,81 @@ def main(env: Env) -> int:
     env.logger.info("Get client from bad network pool:")
     for id in bad_node_ids:
       bad_client_pool.append(env.get_grpc_client_to_outside_node(id))
+    return bad_client_pool, bad_node_ids
+
+
+@test_case("connect_node_to_bad_network")
+def main(env: Env) -> int:
+    sync_port = 20100
+    grpc_port = 50100
+    amount = randrange(1000)
+    update_time = 0.5
+    timeout = 2
+    wait_time = 1
+    transaction_update_time=2
+    max_update_request=10
+
+    env.logger.debug(f"Random amount for test = {amount}")
+
+    bad_client_pool, bad_node_ids = run_bad_nodes(env)
     main_id = Id(sync_port, grpc_port = grpc_port)
     env.logger.info("Start main node with connecting to bad network nodes:")
     # connect to only first node form bad pool, becouse it's IP from good network.
     # If connect to this ids, nodes in bad pool synchron across 2 network card
-    env.start_node(NodeConfig(main_id, nodes=bad_node_ids[0]))
+    env.start_node(NodeConfig(main_id, nodes=[bad_node_ids[0], ]))
+    main_client = env.get_client(ClientType.LEGACY_GRPC, main_id)
+    env.logger.info("Check all nodes:")
+    TEST_CHECK(main_client.connection_test())
+    for client in bad_client_pool:
+      TEST_CHECK(client.connection_test())
+    env.logger.info("All nodes started success.")
+
+    address = main_client.generate_keys(keys_path=f"keys")
+    distributor_address = main_client.load_address(keys_path=get_distributor_address_path())
+    TEST_CHECK_EQUAL(main_client.get_balance(address=address.address, timeout=timeout, wait=wait_time), 0)
+    env.logger.info("New address created.")
+
+    transaction = main_client.transfer(to_address=address.address, amount=amount,
+                              from_address=distributor_address, fee=0, wait=wait_time, timeout=timeout)
+    TEST_CHECK_EQUAL(transaction.status_code, TransactionStatusCode.PENDING)
+    stat = main_client.get_transaction_status(tx_hash=transaction.tx_hash)
+    env.logger.info(f"Wait transaction (transaction_update_time = {transaction_update_time}, max_update_request = {max_update_request}).")
+    request_count = 0
+    while stat.status_code != TransactionStatusCode.SUCCESS:
+      sleep(transaction_update_time)
+      stat = main_client.get_transaction_status(tx_hash=transaction.tx_hash)
+      request_count += 1
+      env.logger.info(f"Wait transaction request_count = {request_count}")
+      if request_count >= max_update_request: return 1
+    env.logger.info("Transaction success.")
+    TEST_CHECK_EQUAL(main_client.get_balance(address=address.address, timeout=timeout, wait=wait_time), amount)
+    env.logger.info("Main client transaction checked success.")
+    for client in bad_client_pool:
+      TEST_CHECK_EQUAL(client.get_balance(address=address.address, timeout=timeout, wait=wait_time), amount)
+    env.logger.info("Test ended success.")
+
+    return 0
+
+
+@test_case("double_connection_in_bad_network")
+def main(env: Env) -> int:
+    sync_port = 20100
+    grpc_port = 50100
+    amount = randrange(1000)
+    update_time = 0.5
+    timeout = 2
+    wait_time = 1
+    transaction_update_time=2
+    max_update_request=10
+
+    env.logger.debug(f"Random amount for test = {amount}")
+
+    bad_client_pool, bad_node_ids = run_bad_nodes(env)
+    main_id = Id(sync_port, grpc_port = grpc_port)
+    env.logger.info("Start main node with connecting to bad network nodes:")
+    # connect to all nodes form bad pool.
+    # For nodes in bad pool synchron across 2 network card
+    env.start_node(NodeConfig(main_id, nodes=bad_node_ids))
     main_client = env.get_client(ClientType.LEGACY_GRPC, main_id)
     env.logger.info("Check all nodes:")
     TEST_CHECK(main_client.connection_test())
